941b8dad019b10768a070fbdc7575c3902a32811
magi-01 <shichenghome@gmail.com>
2026-07-15 13:58:02 -0600

    15/07/26: Temporary backup for v10A3 before red team reviews

--- bleeding/v10A2/include/lancius/lancius_vision_ops.h
+++
@@ -1,8 +0,0 @@
-#ifndef LANCIUS_VISION_OPS_H
-#define LANCIUS_VISION_OPS_H
-#include "lancius/lancius_ir.h"
-
-// Routes heavy 4D math to the dedicated vision engine
-void lancius_execute_vision_op(lancius_node* n);
-
-#endif
